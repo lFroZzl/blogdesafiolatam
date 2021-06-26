6af1c55856faf696836333d6c4102fd8755b8354
FroZz <mafiondres12@gmail.com>
2021-06-26 07:15:46 -0700

    primer commit (todo)

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        post = Post.new(post_params)
+        post.content = post.content.gsub! 'spoiler', ''
+        post.save
+
+        redirect_to root_path
+    end 
+
+    def post_params
+        params.require(:post).permit(:title, :image_url, :content)
+
+    end
+end
